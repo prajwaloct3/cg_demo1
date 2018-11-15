1587fb4d129240cd7b0721336d2471a64e7e3422
prajwaloct3 <n.prajwal@capgemini.com>
2018-11-15 12:57:14 +0530

    commit 2

--- src/com/cg/example/Welcome.java
+++ src/com/cg/example/Welcome.java
@@ -1,7 +1,10 @@
 package com.cg.example;
 
 public class Welcome {
-public static void main(String[] args) {
-	System.out.println("Learning Git");
-}
+	public static void main(String[] args) {
+		System.out.println("Learning Git");
+	}
+	void add(){
+		
+	}
 }
